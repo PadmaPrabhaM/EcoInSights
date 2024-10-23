a547816628334c8e7793c586264bbe501e8ac66e
PadmaPrabhaM <padmaprabhamurugan@gmail.com>
2024-10-23 10:15:09 +0530

    today comit
    comiut

--- Flask-react/server/venv/Scripts/activate.bat
+++ Flask-react/server/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Revathi\Documents\GitHub\Summerproject\Flask-react\server\venv
+set VIRTUAL_ENV=C:\Users\padma\OneDrive\Documents\GitHub\EcoInSights\Flask-react\server\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
